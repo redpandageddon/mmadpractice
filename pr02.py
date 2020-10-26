64d473ffc4e805ebe8bceef24cb2d8ebb4000ab8
redpandageddon <vovanlph@yandex.ru>
2020-10-26 22:08:31 +0300

    requested changes complited

--- pr02.py
+++ pr02.py
@@ -10,21 +10,19 @@ def AbsArrayDifference(A, B):
     return result
 
 def BestArray(arrayAmount, itemAmmount):
-    A = np.zeros((arrayAmount, itemAmmount))
-    B = np.zeros((arrayAmount, 1))
+    arrays = np.zeros((arrayAmount, itemAmmount))
+    sums = np.zeros((arrayAmount, 1))
     
     for i in range(arrayAmount):
-        A[i] = 10 * np.random.random(itemAmmount) - 5
-        B[i] = np.sum(A[i])      
+        arrays[i] = 10 * np.random.random(itemAmmount) - 5
+        sums[i] = np.sum(arrays[i])      
     
-    index = np.argmax(B)
+    index = np.argmax(sums)
     
-    return A[index]
+    return arrays[index]
 
 def EuclidDistance(A, B):
-    diff = np.zeros((A.shape))
     diff = (A - B)**2
-    asd = diff.ravel()
-    result = math.sqrt(sum(asd))
+    result = math.sqrt(sum(diff.ravel()))
     
     return result

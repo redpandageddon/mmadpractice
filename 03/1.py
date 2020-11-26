37aacccfee5cce7170cdeea664cbd41fa378c324
aqaqaqaqaq <z999111000@gmail.com>
2020-11-26 23:47:41 +0300

    Комментарии

--- 03/1.py
+++ 03/1.py
@@ -37,50 +37,67 @@ a = np.random.randint(2, 6, (5, 5))
 print(a)
 
 data = np.random.normal(3, 5, 1000)
+# массив из 1000 элементов c нормальным распределением, мат. ожиданием 3 и среднеквадратическим отклонением 5
 print(data)
 
 data = np.loadtxt('./test.txt', dtype=np.int32)
+#импорт данных из файла и указываем что там целые числа
 print(data)
 
+#загрузка одномерных данных 
 data = scipy.io.loadmat('./1D/var1.mat')
+#ищем(узнаем) ключи
 data.keys()
+#извлекаем данные
 data=data['n']
 
 dataMax=np.max(data)
+#расчет максимального
 print(dataMax)
 
 dataMin=np.min(data)
+#расчет минимального
 print(dataMin)
 
 dataMedian=np.median(data)
+#расчет медианы
 print(dataMedian)
 
 dataMean=np.mean(data)
+#расчет среднего арифметического
 print(dataMean)
 
 dataVar=np.var(data)
+#расчет дисперсии
 print(dataVar)
 
 dataStd=np.std(data)
+#расчет среднеквадратичного отклонения
 print(dataStd)
 
 plt.plot(data)
+#график
 plt.show()
 
+#значение массива, среднее значение и дисперсия на одном графике
 mean = np.mean(data) * np.ones(len(data))
 var = np.var(data) * np.ones(len(data))
 plt.plot(data, 'b-', mean, 'r-', mean-var, 'g--', mean+var, 'g--')
 plt.grid()
 plt.show()
 
+#сетка + количество столбцов на диаграмме
 plt.hist(data, bins=20)
+#показываем сетку на графике
 plt.grid()
 plt.show()
 
+#одномерная матрица to одномерный массив затем выполняем корреляцию и строим график
 data = np.ravel(data)
 cor = autocorrelate(data)
 plt.plot(cor)
 plt.show()
+
 
 datadata = scipy.io.loadmat('./ND/var1.mat')
 datadata.keys()
@@ -91,9 +108,9 @@ corr_matrix = np.zeros((n,n))
 
 for i in range(0,n):
     for j in range(0,n):
-        a1 = datadata[:,i]
-        a2 = datadata[:,j]
-        corr_matrix[i,j]=np.corrcoef(a1,a2)[0,1]
+        a1 = datadata[:,i]   
+        a2 = datadata[:,j]   
+        corr_matrix[i,j]=np.corrcoef(a1,a2)[0,1]    
 
 np.set_printoptions(precision=2)
 print(corr_matrix)

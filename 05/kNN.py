2440871c349b13ee25a30b58f3e9661381b372d9
aqaqaqaqaq <z999111000@gmail.com>
2020-12-03 20:56:09 +0300

    Corrected notes №2

--- 05/kNN.py
+++ 05/kNN.py
@@ -6,19 +6,12 @@ def k_nearest(X, k, obj):
     distances=np.zeros(sub_X.shape[0])
     for i in range(sub_X.shape[0]):
         distances = [dist(item, obj) for item in sub_X]
-    randomArray=np.zeros(k)
-    for i in range(k):
-            j = np.argmin(distances)
-            randomArray[i] = X[j,2]
-            distances[j]= math.inf
-            
-    nearest_classes=np.ones(k)
-    for i in range(k):
-            nearest_classes[i]=randomArray[i]
-            
+        
+    index = np.argsort(distances, axis = 0)
+    nearest_classes=[X[index[item],2] for item in range(k)]
+    
     unique, counts = np.unique(nearest_classes, return_counts=True)
     object_class = unique[np.argmax(counts)]
-
     return object_class
 
 def dist(p1, p2):
